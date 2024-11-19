1bc4c3c5134d6d31ad7fb5f1dc192206222016ee
PraveenDangwani <PraveenDangwani07@gmail.com>
2024-11-20 03:39:10 +0530

    removed comments

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders the LocationWidget', () => {
   render(<App />);
-  // Check if the heading "Locations" is rendered
   const headingElement = screen.getByText("Locations");
   expect(headingElement).toBeInTheDocument();
 });
